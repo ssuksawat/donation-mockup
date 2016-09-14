369921312a483e93e514f84dd0c2f2e05f8eb9f5
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-09-13 22:58:45 -0700

    add compression

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 
 module.exports = (app, config) => {
   app.use(morgan(config.logLevel));
+  app.use(compression());
   app.use(express.static(`${config.rootPath}/public`));
 };
